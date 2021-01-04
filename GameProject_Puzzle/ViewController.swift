2474b714183cec8ee05a09e00edf2589bffd55a6
Igor360 <igor.dawidencko@gmail.com>
2021-01-04 12:19:50 -0800

    Initial Commit

---
+++ GameProject_Puzzle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameProject_Puzzle
+//
+//  Created by Ihor on 04.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
